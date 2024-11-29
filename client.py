7f7ea0ec9ec79f726aba2915462c54ebe9e1fe2e
Ibragim <164409199+Frengocode@users.noreply.github.com>
2024-11-29 20:38:53 +0500

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 import grpc
 from protos import todo_pb2_grpc
 
-
 async def grpc_todo_client():
     channel = grpc.aio.insecure_channel("127.0.0.1:50051")
     client = todo_pb2_grpc.TodoServiceStub(channel)
